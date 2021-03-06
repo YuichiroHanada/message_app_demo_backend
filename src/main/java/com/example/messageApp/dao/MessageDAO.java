1bb60b2113c2c363ec9610e43c0e5ffeadccc608
hanadayuuichirou <hanada.yuichiro@win-techno.com>
2021-03-06 16:19:11 +0900

    change messageDAO

--- src/main/java/com/example/messageApp/dao/MessageDAO.java
+++ src/main/java/com/example/messageApp/dao/MessageDAO.java
@@ -3,5 +3,9 @@ package com.example.messageApp.dao;
 import com.example.messageApp.domain.MessageDO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MessageDAO extends JpaRepository<MessageDO, Integer> {
+
+    List<MessageDO> getAllByRoomId(Integer roomId);
 }
